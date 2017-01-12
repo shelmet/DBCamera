90c3e812e5765c9ac891b15eb90075a96e0a184f
Pavel Kotenko <StanK23@users.noreply.github.com>
2017-01-12 13:27:11 +0600

    Update DBCameraCollectionViewController.m

--- DBCamera/Controllers/DBCameraCollectionViewController.m
+++ DBCamera/Controllers/DBCameraCollectionViewController.m
@@ -71,11 +71,10 @@
     [item.itemImage setImage:nil];
     
     if ( _items.count > 0) {
-        __weak DBCollectionViewCell *blockItem = item;
-        [[[DBLibraryManager sharedInstance] defaultAssetsLibrary] assetForURL:(NSURL *)_items[indexPath.item]  resultBlock:^(ALAsset *asset) {
-            UIImage *image = [UIImage imageWithCGImage:[asset aspectRatioThumbnail]];
-            [blockItem.itemImage setImage:image];
-        } failureBlock:nil];
+        ALAsset *asset = _items[indexPath.item];
+        CGImageRef thumbnailImageRef = [asset thumbnail];
+        UIImage *thumbnail = [UIImage imageWithCGImage:thumbnailImageRef];
+        item.itemImage.image = thumbnail;
     }
     
     return item;
@@ -90,7 +89,7 @@
 
 - (void) collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    [_collectionControllerDelegate collectionView:collectionView itemURL:(NSURL *)_items[indexPath.item]];
+    [_collectionControllerDelegate collectionView:collectionView itemURL:(NSURL *)[[asset defaultRepresentation] url]];
 }
 
 @end
